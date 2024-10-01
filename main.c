b7a3b340be3907e48c087e562bb01c4f7c22e103
ABDELMOUNAIM <abdelmounaimoub12@gmail.com>
2024-10-01 16:46:48 +0200

    V2

--- main.c
+++ main.c
@@ -1,108 +1,153 @@
-// Online C compiler to run C program online
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
-int main() {
-    char titre[100][20];
-    char auteur[100][20];
-    float prix [100];
-    int quantite[100];
-    int choix,x;
-     int    found;
-      int i;
-      char nomm[50];
-       int    newq;
+int main()
+{
+
+        int choix;
+
+
+        char t[100][20];
+        char a[100][20];
+        float prix[100];
+        int quantite[100];
+
+        char nom[50];
+        int newq;
+        int found = 0;
+
         char sup[50];
-    do{
-printf("Gestion de Stock dans une Librairie\n");
+        int x = 0;
+        int f=0;
+        int i;
 
-        printf("1. Ajouter un Livre au Stock:\n");
-        printf("2. Afficher Tous les Livres Disponibles:\n");
-        printf("3. Mettre à Jour la Quantité d'un Livre:\n");
-        printf("4. Supprimer un Livre du Stock\n");
-        printf("5. Afficher le Nombre Total de Livres en Stock:\n");
-        printf("6. Afficher le nombre total de livres en stock:\n");
-        printf("7. Quitter\n");
-        printf("entrez votre choix :");
+        char Rech_a[50];
+        char Rech_t[50];
+
+        while(choix <= 7 || choix >= 7){
+
+        printf("1. Ajoutez un livre.\n");
+        printf("2. Afficher tous les livres.\n");
+        printf("3. Rechercher un livre.\n");
+        printf("4. Mettre a jour la quantite d'un livre.\n");
+        printf("5. Supprimer un livre du Stock\n");
+        printf("6. Afficher le nombre total de livres.\n");
+        printf("7. quitter le programm.\n");
+
+        printf("entrez un choix :\n");
         scanf("%d", &choix);
 
-        //***************************************************
-        switch(choix)
-        {
-         case 1: printf("\nentrez le titer de livere %d:",x+1);
-                   scanf("%s",&titre[x]);
-                   printf("\nentrez le auteur de livere :");
-                   scanf("%s",&auteur[x]);
-                   printf("\nentrez le prix de livere :");
-                   scanf("%f",&prix[x]);
-                   printf("\nentrez la quantite de livere :");
-                   scanf("%d",&quantite[x]);
-                   x++;
-           break;
-         case 2 :
-
-          for(i=0 ; i<x ; i++){
-              printf("****************************** \n");
-              printf("%s \n", titre[i]);
-              printf("%s \n", auteur[i]);
-              printf("%.2f \n", prix[i]);
-              printf("%d \n", quantite[i]);
-          }
-          break;
-         case 3 :
 
 
-         found = 0;
-         printf("entrer le nom de livre à mis-jour");
-         scanf("%s", &nomm);
-         for(i = 0; i < x; i ++)
-         {
-             if(strcmp(nomm, titre[i]) == 0)
-             {
-                 printf("entrer le stock a ajouté: ");
-                 scanf("%d", &quantite[i]);
-                 printf("stock à été mis-à-jouré\n");
-                 found++;
-                 break;
-             }
-         }
-         if(found == 0)
-         {
-         printf("\nlivre non trouvé\n");
-         }
-          break;
+    switch (choix){
+case 1:
+    printf("entrez le titre du livre %d : ", x + 1);
+    scanf("%s", &t[x]);
+    printf("entrez l'auteur : ");
+    scanf("%s", &a[x]);
+    printf("entrez le prix : ");
+    scanf("%f", &prix[x]);
+    printf("entrez quantite : ");
+    scanf("%d", &quantite[x]);
+    printf("livre ajoute avec succes !\n\n\n\n");
+    x++;
+    break;
 
-          case 4 :
+case 2:
 
-          found=0;
-          printf("Entrer le titre de livre que vous voulez supprimer:\n");
-          scanf("%s", &sup[50]);
-          for(i = 0; i < x; i ++)
-         {
-             if(strcmp(sup, titre[i]) == 0)
-             {
-                 strcpy(titre[i],titre[x-1]);
-                 strcpy(auteur[i],auteur[x-1]);
-                 prix[i]=prix[x-1];
-                 quantite[i]=quantite[x-1];
-                 x--;
-                found=1;
-                printf("Le livre est supprime\n");
-                break;
-             }
+    for(int i = 0; i < x; i++){
+     printf("****************\n");
+     printf("%s\n", t[i]);
+     printf("%s\n", a[i]);
+     printf("%.2f\n", prix[i]);
+     printf("%d", quantite[i]);
+    }
 
-         }
-          if(!found){
-              printf("Le titre non trouver\n");
-          } break;
+    break;
+
+case 3:
+
+
+    printf("entrez le nom de livre a mise ajour");
+    scanf("%s", &nom);
+
+    for(int i=0 ; i < x ; i++){
+        if(strcmp(nom, t[i]) == 0){
+            printf("entrez le stock a ajouter :\n");
+            scanf("%d", &quantite[i]);
+            printf("stock est mis a jour");
+            found++;
+            break;
         }
-    }while(choix!=7);
+    }
+    if(found == 0) printf("\nlivre non trouve\n");
+
+    break;
+
+
+case 4:
+
+
+    printf("entrez le titre que vous voulez supprimer: \n");
+    scanf("%s", &sup[50]);
+
+    for ( int i = 0; i < x; i++){
+
+        if(strcmp(sup, t[i])==0){
+            strcpy(t[i],t[x-1]);
+            strcpy(a[i],a[x-1]);
+            prix[i]=prix[x-1];
+            quantite[i]=quantite[x-1];
+            x--;
+            found=1;
+            printf("le livre est supprimer\n");
+            break;
+        }
+    }
 
 
 
+    break;
+
+case 5:
+    found=0;
+    printf("entrez l'auteur : ");
+    scanf("%s", &Rech_a);
+    printf("entrez le titre : ");
+    scanf("%s", &Rech_t);
+    printf("%d :\n", x);
+
+    for ( int i = 0; i < x; i++){
+        printf("%s :  %s :\n", t[i], a[i]);
+        if (strcmp(Rech_t, t[i])==0 && strcmp(Rech_a, a[i])==0){
+            printf("%s\n", t[i]);
+            printf("%s\n", a[i]);
+            printf("%.2f\n", prix[i]);
+            printf("%d\n", quantite[i]);
+            found=1;
+            break;
+        }
+    }
+        if(found==0) printf("le livre n'est pas disponible");
 
 
 
+    break;
 
+case 6:
+    for(i=0 ; i<x ; i++){
+        f = f + quantite[i];
+    }
+    printf("le nombre de livres est %d \n", f);
+    break;
+
+case 7:
+    printf("vous avez quittez :\n");
+    return 0;
+
+
+    default : printf("\n\n\n\n\n\nnumber incorrect. entrez un autre livre\n\n\n\n\n\n");
+}
+}
     return 0;
 }
